22062d566bbabdcb6b64168b7bc97cca6bef1b99
Fernando Parra <thefer767@gmail.com>
2023-02-11 19:49:12 -0400

    Rutas protegidas

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -11,27 +11,27 @@ import { deletecollection, newCarpeta, showcolecction } from '../controllers/car
 //endpoints para users
 router.post('/signup',signUp)
 router.post('/signin',signIn)
-router.post('/finduser',FindUser)
-router.post('/deleteuser',deleteUser)
+router.post('/finduser',passport.authenticate('jwt', {session: false}),FindUser)
+router.post('/deleteuser',passport.authenticate('jwt', {session: false}),deleteUser)
 
 //enpoints para notes
-router.post('/newnote',newNote)
+router.post('/newnote',passport.authenticate('jwt', {session: false}),newNote)
 
 
 router.post('/shownotes',passport.authenticate('jwt', {session: false}),showNotes)
 
 
 
-router.post('/showdetails',showDetails)
-router.post('/edit',editContent)
-router.post('/delete',deleteNote)
-router.post('/addnote',AddtoColecction)
-router.post('/showcarpet',shownotesinacollection)
+router.post('/showdetails',passport.authenticate('jwt', {session: false}),showDetails)
+router.post('/edit',passport.authenticate('jwt', {session: false}),editContent)
+router.post('/delete',passport.authenticate('jwt', {session: false}),deleteNote)
+router.post('/addnote',passport.authenticate('jwt', {session: false}),AddtoColecction)
+router.post('/showcarpet',passport.authenticate('jwt', {session: false}),shownotesinacollection)
 
 //enpoints para carpetas
-router.post('/newcarpet',newCarpeta)
-router.post('/showcolecction',showcolecction)
-router.post('/deletecollection',deletecollection)
+router.post('/newcarpet',passport.authenticate('jwt', {session: false}),newCarpeta)
+router.post('/showcolecction',passport.authenticate('jwt', {session: false}),showcolecction)
+router.post('/deletecollection',passport.authenticate('jwt', {session: false}),deletecollection)
 
 
 
