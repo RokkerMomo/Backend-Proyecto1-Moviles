e431818f39c977f6632a05fbc9f37d15671f4f27
Fernando Parra <thefer767@gmail.com>
2023-02-11 22:05:23 -0400

    Se arreglo un bug

--- dist/routes/auth.routes.js
+++ dist/routes/auth.routes.js
@@ -12,18 +12,18 @@ const carpetas_controller_1 = require("../controllers/carpetas.controller");
 //endpoints para users
 router.post('/signup', user_contoller_1.signUp);
 router.post('/signin', user_contoller_1.signIn);
-router.post('/finduser', user_contoller_1.FindUser);
-router.post('/deleteuser', user_contoller_1.deleteUser);
+router.post('/finduser', passport_1.default.authenticate('jwt', { session: false }), user_contoller_1.FindUser);
+router.post('/deleteuser', passport_1.default.authenticate('jwt', { session: false }), user_contoller_1.deleteUser);
 //enpoints para notes
-router.post('/newnote', notas_controller_1.newNote);
+router.post('/newnote', passport_1.default.authenticate('jwt', { session: false }), notas_controller_1.newNote);
 router.post('/shownotes', passport_1.default.authenticate('jwt', { session: false }), notas_controller_1.showNotes);
-router.post('/showdetails', notas_controller_1.showDetails);
-router.post('/edit', notas_controller_1.editContent);
-router.post('/delete', notas_controller_1.deleteNote);
-router.post('/addnote', notas_controller_1.AddtoColecction);
-router.post('/showcarpet', notas_controller_1.shownotesinacollection);
+router.post('/showdetails', passport_1.default.authenticate('jwt', { session: false }), notas_controller_1.showDetails);
+router.post('/edit', passport_1.default.authenticate('jwt', { session: false }), notas_controller_1.editContent);
+router.post('/delete', passport_1.default.authenticate('jwt', { session: false }), notas_controller_1.deleteNote);
+router.post('/addnote', passport_1.default.authenticate('jwt', { session: false }), notas_controller_1.AddtoColecction);
+router.post('/showcarpet', passport_1.default.authenticate('jwt', { session: false }), notas_controller_1.shownotesinacollection);
 //enpoints para carpetas
-router.post('/newcarpet', carpetas_controller_1.newCarpeta);
-router.post('/showcolecction', carpetas_controller_1.showcolecction);
-router.post('/deletecollection', carpetas_controller_1.deletecollection);
+router.post('/newcarpet', passport_1.default.authenticate('jwt', { session: false }), carpetas_controller_1.newCarpeta);
+router.post('/showcolecction', passport_1.default.authenticate('jwt', { session: false }), carpetas_controller_1.showcolecction);
+router.post('/deletecollection', passport_1.default.authenticate('jwt', { session: false }), carpetas_controller_1.deletecollection);
 exports.default = router;
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,7 +7,7 @@ const router = Router()
 import { deleteUser, FindUser, signIn,signUp } from '../controllers/user.contoller';
 import { AddtoColecction, deleteNote, editContent, newNote, showDetails, showNotes, shownotesinacollection } from "../controllers/notas.controller";
 import { deletecollection, newCarpeta, showcolecction } from '../controllers/carpetas.controller';
-
+ 
 //endpoints para users
 router.post('/signup',signUp)
 router.post('/signin',signIn)
@@ -18,9 +18,7 @@ router.post('/deleteuser',passport.authenticate('jwt', {session: false}),deleteU
 router.post('/newnote',passport.authenticate('jwt', {session: false}),newNote)
 
 
-router.post('/shownotes',passport.authenticate('jwt', {session: false}),showNotes)
-
-
+router.post('/shownotes',passport.authenticate('jwt', {session: false}),showNotes)  
 
 router.post('/showdetails',passport.authenticate('jwt', {session: false}),showDetails)
 router.post('/edit',passport.authenticate('jwt', {session: false}),editContent)
